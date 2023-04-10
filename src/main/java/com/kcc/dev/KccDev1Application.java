83c976ae4db68befb17fd7564d1eb1776c5c6ea3
Jeong Younghun <Jeong Younghun@DESKTOP-Q182NU8>
2023-04-10 16:08:53 +0900

    init

---
+++ src/main/java/com/kcc/dev/KccDev1Application.java
@@ -0,0 +1,13 @@
+package com.kcc.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KccDev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KccDev1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcc/dev/KccDev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcc.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KccDev1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
